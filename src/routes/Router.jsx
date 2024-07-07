bf7602cfb3cd2dc63734ead3c2b09c06043e0cda
devskinshy <yyshgk12@gmail.com>
2024-07-07 22:19:48 +0900

    init

---
+++ src/routes/Router.jsx
@@ -0,0 +1,15 @@
+import {useRoutes} from "react-router-dom";
+import HomePage from "../pages/HomePage.jsx";
+import MainLayout from "../layouts/MainLayout.jsx";
+
+export default function Router() {
+  return useRoutes([
+    {
+      path: '/',
+      element: <MainLayout/>,
+      children: [
+        { index: true, element: <HomePage/> }
+      ],
+    },
+  ])
+}
---
+++ src/theme/ThemeProvider.jsx
@@ -0,0 +1,10 @@
+import { createTheme, ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
+import {CssBaseline} from "@mui/material";
+
+export default function ThemeProvider({ children }) {
+  const theme = createTheme();
+  return <MuiThemeProvider theme={theme}>
+    <CssBaseline />
+    {children}
+  </MuiThemeProvider>;
+}
