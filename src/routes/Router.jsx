cc6d6c49e7283b7efcf84ee2f85f083bf862a384
devskinshy <yyshgk12@gmail.com>
2024-07-14 22:40:07 +0900

    layout setting

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,6 @@
 import {useRoutes} from "react-router-dom";
 import HomePage from "../pages/HomePage.jsx";
-import MainLayout from "../layouts/MainLayout.jsx";
+import MainLayout from "../layouts/MainLayout/index.jsx";
 
 export default function Router() {
   return useRoutes([
