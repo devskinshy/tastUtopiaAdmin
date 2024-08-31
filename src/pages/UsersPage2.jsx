1a641384a7db16be19fc2ad2419ba0e81882d17f
SJY <sinkiller108@naver.com>
2024-08-31 20:43:49 +0900

    유저 리스트 1차 커밋

---
+++ src/pages/UsersPage2.jsx
@@ -0,0 +1,135 @@
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogActions from '@mui/material/DialogActions';
+
+import React, { useEffect, useState } from "react";
+
+import Button from '@mui/material/Button';
+
+import axios from "axios";
+
+
+export function test1(){
+  return alert("test1");  
+
+};
+
+export function test2(temp){
+  alert("test2"+temp);
+};
+
+export default function BasicTable() {
+  const [status, setStatus] = useState();
+  const [code, setCode] = useState();
+  const [rows1, setRows] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const [error, setError] = useState(false);
+  const url ="http://3.35.212.57:8080/user/users";
+
+  //컴포넌트가 마운트 됐을때  
+   useEffect(() => {
+      axios({
+        method: 'get'
+        ,url: url
+        //,params:{}
+        //,headers:{}
+      })
+      .then((response)=>{
+        const result = response.data;
+        alert(result.data);
+        alert(result.status);
+        alert(result.code);
+      })
+      .catch((error)=>{
+        console.log("Store Error:: ", error.message);
+        setRows(null);
+        setError(true);
+      })
+
+   },[])
+  
+   const handleClose = () => {
+    setError(false);
+    navigate("/", { replace: true });
+  };
+  
+  if(false){
+    return (
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>사용자ID</TableCell>
+              <TableCell align="right">별명</TableCell>
+              <TableCell align="right">이메일</TableCell>
+              <TableCell align="right">생성일자</TableCell>
+              <TableCell align="right">사용자종류</TableCell>
+              <TableCell align="right">사용여부</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">{row.name}</TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
+  if(false){
+    return (
+      alert('100'),
+      <Dialog
+        open={error}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"예기치 못한 에러가 발생했습니다."}
+        </DialogTitle>
+        <DialogActions>
+          <Button onClick={handleClose}>확인</Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+
+  if (loading) {
+    return (
+      alert('5200'),
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
+  
+}
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,16 +2,22 @@ import {useRoutes} from "react-router-dom";
 import HomePage from "../pages/HomePage.jsx";
 import MainLayout from "../layouts/MainLayout/index.jsx";
 import StorePage from "../pages/StorePage.jsx";
+import BasicTable from "../pages/UsersPage2.jsx";
+
+
 
 export default function Router() {
   return useRoutes([
+    
     {
       path: '/',
       element: <MainLayout/>,
       children: [
-        { index: true, element: <HomePage/> },
-        { path: 'store', element: <StorePage />}
+        { index: true, element: <HomePage/> }
+        ,{ path: 'store', element: <StorePage />}
+        ,{ path: 'users2', element: <BasicTable />}
       ],
     },
+   
   ])
 }
