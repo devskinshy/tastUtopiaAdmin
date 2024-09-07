c9ac2cb2ba3641c7f2261075662c1c2b1a491fca
liontrout <rlaansgur95@gmail.com>
2024-09-08 02:48:05 +0900

    feat: 에러 처리

--- src/pages/User/UserApi.jsx
+++ src/pages/User/UserApi.jsx
@@ -15,63 +15,74 @@ export default function UserApi() {
   const [error, setError] = useState('');
   const navigate = useNavigate();
 
-  const getUserData = (url) => {
+  const getUserData = () => {
+    setLoading(true);
+    setError('');
+
     // 나중에 따로 빼면 좋을 듯
-    axios(url)
+    axios('http://3.35.212.57:8080/user/users')
     .then(response => {
       // console.log(response)
-      const result = response.data?.data;
-      (!result || result.length > 0) ? setUserData(result) : setError(`데이터가 존재하지 않습니다.`);
+      const result = response.data;
+      if ( result.status !== 200 ) {  // 매직 넘버
+        throw new Error(`에러가 발생했습니다.`);
+      }
+
+      const hasData = result?.data && result.data.length > 0;
+      if ( !hasData ) {
+        throw new Error(`데이터가 존재하지 않습니다.`);
+      }
+
+      setUserData(result.data);
     })
     .catch(err => {
       console.error(err);
-      setError(`에러가 발생했습니다.`); // error code, message 검토 필요
+      setError(err.message || `에러가 발생했습니다.`); // error code, message 검토 필요
     })
     .finally(() => {
       setLoading(false);
     });
   };
 
-  const retryUserData = () => {
-    setLoading(true);
-    setError('');
-    getUserData('http://3.35.212.57:8080/user/users');
-  };
-
   useEffect(() => {
-    getUserData('http://3.35.212.57:8080/user/users');
+    getUserData();
   }, []);
 
   // 로딩
-  if ( loading ) return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      height="100%"
-    >
-      <CircularProgress />
-    </Box>
-  );
+  if ( loading ) {
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100%"
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
 
   // 에러
-  if ( error ) return (
-    <Alert
-      severity="error"
-      action={
-        <>
-          <Button color="inherit" size="small" onClick={retryUserData}>
-            다시 시도
-          </Button>
-          <Button color="inherit" size="small" onClick={() => navigate(-1)}>
-            뒤로 가기
-          </Button>
-        </>
-      }
-    >
-      {error}
-    </Alert>
-  );
+  // TODO: 정상 응답 이외는 전부 에러로 간주, response 상태에 따른 에러 처리 필요
+  if ( error ) {
+    return (
+      <Alert
+        severity="error"
+        action={
+          <>
+            <Button color="inherit" size="small" onClick={getUserData}>
+              다시 시도
+            </Button>
+            <Button color="inherit" size="small" onClick={() => navigate(-1)}>
+              뒤로 가기
+            </Button>
+          </>
+        }
+      >
+        {error}
+      </Alert>
+    );
+  }
   
   return (
     <UserTable
