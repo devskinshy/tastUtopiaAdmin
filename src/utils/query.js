62089d598d1c24fd2ad51b8a2a86a353006ebea6
devskinshy <yyshgk12@gmail.com>
2024-10-15 23:45:45 +0900

    임시 저장

--- src/utils/query.js
+++ src/utils/query.js
@@ -1,3 +1,14 @@
 import {QueryClient} from "@tanstack/react-query";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 10,
+      gcTime: 1000 * 60 * 10,
+      retry: false,
+    },
+    mutations: {
+      retry: false
+    }
+  }
+});
